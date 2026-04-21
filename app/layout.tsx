4f5e27067f555c2ac3617745743d37bde01e37ff
julien-sketch <julien@picqtures.fr>
2026-04-21 15:58:14 +0200

    Ajout Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Figtree, Instrument_Serif, Syne } from "next/font/google";
 import "./globals.css";
 
@@ -34,9 +35,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MPL9N94V');
+          `}
+        </Script>
+      </head>
       <body
         className={`${figtree.variable} ${syne.variable} ${instrumentSerif.variable} min-h-screen bg-[#faf6ef] text-[#1f1a16] [font-family:var(--font-farmasi-body)] antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MPL9N94V"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
